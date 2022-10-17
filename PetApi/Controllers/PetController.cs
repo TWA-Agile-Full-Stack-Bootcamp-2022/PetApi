e1027ece305fe53e13d2ee5db7c143321a240a04
KUNWEI <kwei@thoughtworks.com>
2022-10-17 11:49:07 +0800

    feat: create find pets by type, priceRange, color api

--- PetApi/Controllers/PetController.cs
+++ PetApi/Controllers/PetController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PetApi.Models;
 
@@ -45,6 +44,24 @@ namespace PetApi.Controllers
             var currentPet = pets.First(pet => pet.Name.Equals(pet.Name));
             currentPet.Price = pet.Price;
             return currentPet;
+        }
+        
+        [HttpGet("findPetsByType")]
+        public IEnumerable<Pet> FindPetsByType(string type)
+        {
+            return pets.Where(pet => pet.Type.Equals(type));
+        }
+        
+        [HttpGet("findPetsByPriceRange")]
+        public IEnumerable<Pet> FindPetsByPriceRange(int minPrice, int maxPrice)
+        {
+            return pets.Where(pet => pet.Price >= minPrice && pet.Price <= maxPrice);
+        }
+
+        [HttpGet("findPetsByColor")]
+        public IEnumerable<Pet> FindPetsByColor(string color)
+        {
+            return pets.Where(pet => pet.Color.Equals(color));
         }
         
         [HttpGet("resetPets")]
