53ea85c36c275abacd72d89e1de81bd5770b6d45
KUNWEI <kwei@thoughtworks.com>
2022-10-17 11:26:57 +0800

    feat: create get all pets api

--- PetApi/Controllers/PetController.cs
+++ PetApi/Controllers/PetController.cs
@@ -11,13 +11,19 @@ namespace PetApi.Controllers
     [Route("api")]
     public class PetController : ControllerBase
     {
-        private List<Pet> pets = new List<Pet>();
+        private static List<Pet> pets = new List<Pet>();
 
         [HttpPost("addPet")]
         public Pet AddGet(Pet pet)
         {
             pets.Add(pet);
             return pet;
+        }
+        
+        [HttpGet("getAllPets")]
+        public List<Pet> GetPets()
+        {
+            return pets;
         }
     }
 }
--- PetApiTest/PetApiTest.cs
+++ PetApiTest/PetApiTest.cs
@@ -18,8 +18,7 @@ namespace PetApiTest
         public async Task Should_add_new_pets_successful()
         {
             // Given
-            TestServer testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
-            HttpClient httpClient = testServer.CreateClient();
+            HttpClient httpClient = GenerateHttpClient();
             Pet pet = new Pet(name: "Baymax", type: "dog", color: "white", price: 1000);
             StringContent stringContent =
                 new StringContent(JsonConvert.SerializeObject(pet), Encoding.UTF8, "application/json");
@@ -29,6 +28,35 @@ namespace PetApiTest
             response.EnsureSuccessStatusCode();
             Pet actualPet = JsonConvert.DeserializeObject<Pet>(await response.Content.ReadAsStringAsync());
             Assert.Equal(pet, actualPet);
+        }
+
+        [Fact]
+        public async Task Should_Return_All_Pets_When_Get_All_Pets()
+        {
+            // given
+            HttpClient httpClient = GenerateHttpClient();
+
+            Pet baymaxDog = new Pet(name: "Baymax", type: "dog", color: "white", price: 1000);
+            await httpClient.PostAsync("api/addPet",
+                new StringContent(JsonConvert.SerializeObject(baymaxDog), Encoding.UTF8, "application/json"));
+            Pet oldblackCat = new Pet(name: "Old Black", type: "cat", color: "black", price: 500);
+            await httpClient.PostAsync("api/addPet",
+                new StringContent(JsonConvert.SerializeObject(oldblackCat), Encoding.UTF8, "application/json"));
+
+            // when
+            HttpResponseMessage response = await httpClient.GetAsync("api/getAllPets");
+
+            // then
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            List<Pet> actualPets = JsonConvert.DeserializeObject<List<Pet>>(responseBody);
+            Assert.Equal(new List<Pet>() { baymaxDog, oldblackCat }, actualPets);
+        }
+
+        private static HttpClient GenerateHttpClient()
+        {
+            TestServer testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            return testServer.CreateClient();
         }
     }
 }
