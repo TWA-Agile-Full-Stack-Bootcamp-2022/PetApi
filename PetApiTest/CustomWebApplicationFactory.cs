46fb1b748317fc230249dba5c08b8e406d65a2b7
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-19 00:40:24 +0800

    test: install mvc testing nuget and create hello world test case

---
+++ PetApiTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace PetApiTest
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup>
+        where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services => { });
+        }
+    }
+}
---
+++ PetApiTest/PetControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using PetApi;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PetApiTest
+{
+    public class PetControllerTest : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient client;
+        private readonly CustomWebApplicationFactory<Program> factory;
+
+        public PetControllerTest(CustomWebApplicationFactory<Program> factory)
+        {
+            this.factory = factory;
+            client = factory.CreateClient(new WebApplicationFactoryClientOptions { });
+        }
+
+        [Fact]
+        public async Task Should_return_hello_world_when_get_root_path()
+        {
+            //given
+            //when
+            var response = await client.GetAsync("/Hello");
+
+            var returnString = await response.Content.ReadAsStringAsync();
+            //then
+            Assert.Equal("Hello World", returnString);
+        }
+    }
+}
